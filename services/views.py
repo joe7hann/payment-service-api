30880303abc7b19d36bd6a832864bde6e390dbbc
Johann <joe7hann@gmail.com>
2022-12-29 21:40:42 -0500

    permission modifed just for now

--- services/views.py
+++ services/views.py
@@ -6,7 +6,7 @@ from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 #class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
-    permission_classes = [permissions.IsAdminUser, permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAdminUser, permissions.IsAuthenticated]
     serializer_class = ServiceSerializer
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
     
